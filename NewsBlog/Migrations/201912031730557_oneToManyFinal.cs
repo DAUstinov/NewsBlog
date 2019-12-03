e6be522d7827d531b931405491022ae3eb898f03
Dmitriy <dimaust2012@mail.ru>
2019-12-03 23:19:51 +0300

    CRUDCategory

---
+++ NewsBlog/Migrations/201912031730557_oneToManyFinal.cs
@@ -0,0 +1,18 @@
+﻿namespace NewsBlog.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class oneToManyFinal : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.BlogItems", "Category");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.BlogItems", "Category", c => c.String());
+        }
+    }
+}
---
+++ NewsBlog/Migrations/201912031900129_oneToManyFinalv0.1.cs
@@ -0,0 +1,26 @@
+﻿namespace NewsBlog.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class oneToManyFinalv01 : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.BlogItems", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.BlogItems", new[] { "CategoryId" });
+            AlterColumn("dbo.BlogItems", "CategoryId", c => c.Int(nullable: false));
+            CreateIndex("dbo.BlogItems", "CategoryId");
+            AddForeignKey("dbo.BlogItems", "CategoryId", "dbo.Categories", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.BlogItems", "CategoryId", "dbo.Categories");
+            DropIndex("dbo.BlogItems", new[] { "CategoryId" });
+            AlterColumn("dbo.BlogItems", "CategoryId", c => c.Int());
+            CreateIndex("dbo.BlogItems", "CategoryId");
+            AddForeignKey("dbo.BlogItems", "CategoryId", "dbo.Categories", "Id");
+        }
+    }
+}
